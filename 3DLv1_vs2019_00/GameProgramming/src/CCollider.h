834eb4696545d876176a0559d37f51741ea0db52
Nishihara-shuya-Fukuyama <gc23005@anabuki-net.ne.jp>
2024-10-10 13:47:57 +0900

    34

--- 3DLv1_vs2019_00/GameProgramming/src/CCollider.h
+++ 3DLv1_vs2019_00/GameProgramming/src/CCollider.h
@@ -28,6 +28,10 @@ public:
 	};
 	//デフォルトコンストラクタ
 	CCollider();
+	//CollisionTriangleSphere(三角コライダ, 球コライダ, 調整値)
+//retrun:true（衝突している）false(衝突していない)
+//調整値:衝突しない位置まで戻す値
+	static bool CollisionTriangleSphere(CCollider* triangle, CCollider* sphere, CVector* adjust);
 	//衝突判定
 //Collision(コライダ1, コライダ2)
 //retrun:true（衝突している）false(衝突していない)
